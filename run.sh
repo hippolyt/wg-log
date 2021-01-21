d1716518ac086c93686d82d533fe81d0c616ca95
kris <kris@pop-os.localdomain>
2021-01-21 09:27:19 +0100

    test

--- run.sh
+++ run.sh
@@ -8,15 +8,19 @@ git pull
 
 
 echo "installing"
+# Startup initialization
 sudo cp wglog.desktop /etc/xdg/autostart/wglog.desktop 
 sudo chmod 644 /etc/xdg/autostart/wglog.desktop 
-
+# Dependencies
+sudo apt update
+sudo apt install -y jq
 
 echo "press any key to cancel startup"
 read -n 1 -t 10 myKey
 if [ $? = 0 ] ; then
     exit;
 else
+    jq '.profile.exit_type= "Normal" | .profile.exited_cleanly = true' ~pi/.config/chromium/Default/Preferences > ~pi/.config/chromium/Default/Preferences
     chromium-browser --kiosk --disable-infobars --app=index.html
 fi
 
